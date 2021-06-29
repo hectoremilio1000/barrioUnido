df8e82594dee027a58d9646fcc366aa63989e356
hectoremilio1000 <hectoremilio1000@gmail.com>
2021-06-28 21:18:43 -0500

    n1

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,9 @@ module.exports = {
     title: "barrioUnido",
   },
   plugins: [{
-    resolve: `gatsby-plugin-s3`,
-    options: {
-      bucketName: 'mybucketname.com',
-      acl: null
-    }}],
+  resolve: `gatsby-plugin-s3`,
+  options: {
+    bucketName: 'barriounido91c230005bd44a3ab921930ad28f53f9161846-dev',
+    acl: null
+  }}],
 };
